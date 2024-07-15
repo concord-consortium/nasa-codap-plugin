2568740197205e68a34113cf93a71fb4b62e1844
Joe Bacal <bacalj@gmail.com>
2024-07-15 10:03:39 -0400

    placeholder tokens, rudimentary styling, temporary debug area

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,12 +19,15 @@ const kDataContextName = "DayLength3PluginData";
 
 export const App = () => {
   const [codapResponse, setCodapResponse] = useState<any>(undefined);
+  const [location, setLocation] = useState<string>("");
+  const [latitude, setLatitude] = useState<string>("");
+  const [longitude, setLongitude] = useState<string>("");
 
   useEffect(() => {
-    initializePlugin({pluginName: kPluginName, version: kVersion, dimensions: kInitialDimensions});
+    initializePlugin({ pluginName: kPluginName, version: kVersion, dimensions: kInitialDimensions });
   }, []);
 
-  const handleCreateData = async() => {
+  const handleCreateData = async () => {
     const existingDataContext = await getDataContext(kDataContextName);
     let createDC, createNC, createI;
     if (!existingDataContext.success) {
@@ -57,27 +60,98 @@ export const App = () => {
     setCodapResponse(result);
   };
 
+  const handleLatitudeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("| lat change: ", event.target.value);
+    setLatitude(event.target.value);
+  };
+
+  const handleLpngitudeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("| long change: ", event.target.value);
+    setLongitude(event.target.value);
+  };
+
+  const handleLocChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("| loc change: ", event.target.value);
+    setLocation(event.target.value);
+  };
+
   return (
     <div className="App">
       <div className="plugin-row">
-        <p>How long is a day?</p>
-        <p>Enter a location or coordinates to retrieve data</p>
+        <p>
+          How long is a day?<br />
+          Enter a location or coordinates to retrieve data
+        </p>
+      </div>
+      <hr />
+      <div className="plugin-row">
+        <label>Location:</label>
+        <input
+          type="text"
+          placeholder="city, state or country"
+          value={location}
+          onChange={handleLocChange}
+        />
+      </div>
+
+      <hr />
+
+      <div className="plugin-row">
+        <label>Latitude:</label>
+        <input
+          type="text"
+          placeholder="latitude"
+          value={latitude}
+          onChange={handleLatitudeChange}
+        />
+      </div>
+
+      <div className="plugin-row">
+        <label>Longitude:</label>
+        <input
+          type="text"
+          placeholder="longitude"
+          value={longitude}
+          onChange={handleLpngitudeChange}
+        />
+      </div>
+
+      <hr />
+      <div className="plugin-row">
+        Attributes
       </div>
       <div className="plugin-row">
-        <label>Location:</label><br/>
-        <input type="text" placeholder="city, state or country" />
+        { /* placeholder */}
+
+        <ul className="attribute-tokens">
+          <li>Day</li>
+          <li>Day Length</li>
+          <li>Rise Hour</li>
+          <li>Set Hour</li>
+          <li>Sunlight Angle</li>
+          <li>Solar Intensity</li>
+          <li>Surface Area</li>
+          <li>Season</li>
+        </ul>
       </div>
-      <div className="buttons">
-        <button onClick={handleCreateData}>
-          Create pets data
+      <div className="plugin-row">
+        <button>
+          Get Data
         </button>
-        <button onClick={handleGetResponse}>
-          See getAllItems response
-        </button>
-        <div className="response-area">
-          <span>Response:</span>
-          <div className="response">
-            {codapResponse && `${JSON.stringify(codapResponse, null, "  ")}`}
+      </div>
+      <div className="temporary">
+        <div className="buttons">
+          <button onClick={handleCreateData}>
+            Create pets data
+          </button>
+          <button onClick={handleGetResponse}>
+            See getAllItems response
+          </button>
+          <div className="response-area">
+            <span>Response:</span>
+            <div className="response">
+              {codapResponse && `${JSON.stringify(codapResponse, null, "  ")}`}
+            </div>
           </div>
         </div>
       </div>
