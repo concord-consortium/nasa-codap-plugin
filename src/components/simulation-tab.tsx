bb34b0adce01783fb4be0460c0e0074c99d50071
pjanik <janikpiotrek@gmail.com>
2024-08-05 22:26:22 +0200

    feat: initialize GRASP Seasons app in Simulation Tab [PT-188059181]

--- src/components/simulation-tab.tsx
+++ src/components/simulation-tab.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ILocation } from "../types";
-import { OrbitSystem } from "./orbit-system";
+import Seasons from "../grasp-seasons/components/seasons";
 
 import "../assets/scss/simulation-tab.scss";
 
@@ -20,36 +20,11 @@ export const SimulationTab: React.FC<SimulationTabProps> = ({
   location
 }) => {
 
-  const handleDayChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDayOfYear(event.target.value);
-  };
-
   return (
     <div className="simulation-tab">
-      <div className="plugin-row day-slider">
-        <label>Day of Year</label>
-        <input
-          type="range"
-          min="0"
-          max="364"
-          value={dayOfYear}
-          onChange={handleDayChange}
-        />
+      <div className="seasons-container">
+        <Seasons />
       </div>
-      <div className="plugin-row sim">
-        <OrbitSystem
-          latitude={parseFloat(latitude) || 0}
-          longitude={parseFloat(longitude) || 0}
-          dayOfYear={parseInt(dayOfYear, 10) || 0}
-        />
-      </div>
-
-      <div className="location-info">
-        <pre>Location Name: {location?.name}</pre>
-        <pre>Latitude: {latitude}</pre>
-        <pre>Longitude: {longitude}</pre>
-      </div>
-
     </div>
   );
 };
