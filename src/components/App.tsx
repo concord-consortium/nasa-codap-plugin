d907f43a7766c470f928b09b7ed9e218abca83c9
Joe Bacal <bacalj@gmail.com>
2024-08-21 10:30:31 -0400

    call selectSelf on body click

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -184,7 +184,7 @@ export const App: React.FC = () => {
 
 
   return (
-    <div className="App">
+    <div className="App" onClick={selectSelf}>
       <Header
         activeTab={activeTab}
         onTabClick={handleTabClick}
